e36085df85ba5bfee659cc65ab1b4be6cebaa823
Justin Oh <justin.oh@kaist.ac.kr>
2020-03-24 20:31:10 +0900

    removed redundancy

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import srgan
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import itertools
 from train_utils import train, test
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset
